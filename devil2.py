f3ee3052df412b34cd82297f2d825f4645880a87
Marcus Aurelianus <37787424+marcus-aurelianus@users.noreply.github.com>
2020-01-14 15:29:41 +0800

    Update devil2.py
    using recursion

--- devil2.py
+++ devil2.py
@@ -7,64 +7,32 @@ maxn=max(lst)
 n=len(bin(maxn)[2:])
 newlst=["0"*(n-len(bin(ele)[2:]))+bin(ele)[2:] for ele in lst]
 
-res=['0']*n
-
-def countzeroOne(lstrino,loc):
-    count0=0
-    count1=0
-    temp1,temp2=[],[]
-    for lst1 in newlst:
-        if lst1[loc]=='0':
-            count0+=1
-            temp1.append(lst1)
+def catchEvil(lstrino,loc):
+    count0,count1=[],[]
+    for ele in lstrino:
+        if ele[n-1-loc]=='1':
+            count1.append(ele)
         else:
-            count1+=1
-            temp2.append(lst1)
-    oinOne,oinTwo=False,False
-    zinOne,zinTwo=False,False
-    max1,max2=0,0
-    for ele in temp1:
-        max1=max(max1,int(ele[loc+1:],2))
-        if ele[loc+1]=='1':
-            oinOne=True
+            count0.append(ele)
+    if len(count0)==0:
+        if loc==0:
+            return 0
         else:
-            zinOne=True
-    for ele in temp2:
-        max2=max(max2,int(ele[loc+1:],2))
-        if ele[loc+1]=='1':
-            oinTwo=True
+            return catchEvil(lstrino,loc-1)
+    elif len(count1)==0:
+        if loc==0:
+            return 0
         else:
-            zinTwo=True
-    return count0,count1,zinOne and oinOne,zinTwo and oinTwo,temp1,temp2,max1>max2
-marker=False
-for i in range(n-1):
-    count0,count1,inOne,inTwo,temp1,temp2,checker=countzeroOne(newlst,i)
-    #print(i,newlst,res,count1,inOne,inTwo)
-    if count1==1:
-        if not marker and count0>0:
-            res[i]='1'
-            marker=True
-        break
-    elif count1==0:
-        continue
+            return catchEvil(lstrino,loc-1)
     else:
-        if count0>0:
-            if not marker:
-                res[i]='1'
-                marker=True
-            if inOne and inTwo:
-                if checker:
-                    marker=False
-                    newlst=temp2
-                else:
-                    marker=False
-                    newlst=temp2
-            if inOne:
-                marker=False
-                newlst=temp2
-            elif inTwo:
-                marker=False
-                newlst=temp1
-    
+        if loc==0:
+            return 1
+        else:
+            return min(catchEvil(count1,loc-1),catchEvil(count0,loc-1))+(1<<loc)
 
-print(int("".join(res),2))       
+
+if  n==0:
+    print(0)
+else:
+    print(int(catchEvil(newlst,n-1)))  
+    

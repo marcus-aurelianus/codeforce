4764df151ade7806e32b6c88283a2de946f99e16
Marcus Aurelianus <37787424+marcus-aurelianus@users.noreply.github.com>
2021-03-13 02:14:08 +0800

    Update binarysearchandSlidingWindow.py

--- EDU105/binarysearchandSlidingWindow.py
+++ EDU105/binarysearchandSlidingWindow.py
@@ -7,18 +7,56 @@ def gift():
         n,m = list(map(int,input().split()))
         boxs = list(map(int,input().split()))
         poss = list(map(int,input().split()))
-        ans = 0 
-        if boxs[0]<0 and poss[0]<0:
-            sb = bisect.bisect_left(boxs,0)
-            sp = bisect.bisect_left(boxs,poss[0])
-            print(sb,sp)
-            ans = max(sb-sp,ans)
-        if boxs[-1]>0 and poss[-1]>0:
-            eb = bisect.bisect_right(boxs,0)
-            ep = bisect.bisect_right(boxs,poss[-1])
-            print(eb,ep)
-            ans = max(ep-eb,ans)
-        yield ans
+        ap = []
+        bp = []
+        am = []
+        bm = []
+        for i in boxs:
+            if i>0:
+                ap.append(i)
+            else:
+                am.append(-1*i)
+        for i in poss:
+            if i>0:
+                bp.append(i)
+            else:
+                bm.append(-1*i)
+        am.reverse()
+        bm.reverse()
+
+        rembox = len(ap)
+        setbox = 0
+        pans = 0
+        for i in range(len(bp)-1,-1,-1):
+            nb = bp[i]
+            while len(ap)>0 and ap[-1]>nb:
+                rembox -= 1
+                ap.pop()
+            pans = max(pans, setbox + i - bisect.bisect_right(bp,nb-rembox) + 1)
+            if len(ap)>0 and nb == ap[-1]:
+                rembox -= 1
+                setbox += 1
+                ap.pop()
+
+        mans = 0
+        ap = am
+        bp = bm
+        rembox = len(ap)
+        setbox = 0
+        for i in range(len(bp)-1,-1,-1):
+            
+            nb = bp[i]
+            while len(ap) > 0 and ap[-1] > nb:
+                rembox -= 1
+                ap.pop()
+            
+            mans = max(mans , setbox + i - bisect.bisect_right(bp,nb-rembox) + 1)
+     
+            if len(ap) > 0 and nb == ap[-1]:
+                rembox -= 1
+                setbox += 1
+                ap.pop()             
+        yield pans+mans
 if __name__ == '__main__':
     t= int(input())
     ans = gift()
